01bf99605172362ee8e596848304faeb584cdd4e
iortega-dev <iortegamancera@gmail.com>
2022-09-22 19:21:46 +0200

    feat: wrap App with PokemonProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Layout from './components/Layout'
+import { PokemonProvider } from './contexts/PokemonContext'
 
 function App() {
-  return <Layout />
+  return (
+    <PokemonProvider>
+      <Layout />
+    </PokemonProvider>
+  )
 }
 
 export default App
