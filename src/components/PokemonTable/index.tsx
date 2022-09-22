6c3a9be0320a23e8d80a3158bec323e529e8a51c
iortega-dev <iortegamancera@gmail.com>
2022-09-22 19:23:22 +0200

    feat: add PokemonTable component

---
+++ src/components/PokemonTable/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import { DataGrid, GridColDef, GridEventListener } from '@mui/x-data-grid'
+import { NamedAPIResource } from '../../types/Common'
+
+interface PokemonTableProps {
+  pokemons: NamedAPIResource[]
+}
+
+const columns: GridColDef[] = [
+  {
+    field: 'name',
+    headerName: 'Nombre',
+    editable: false,
+    flex: 1,
+  },
+  {
+    field: 'url',
+    headerName: 'URL',
+    editable: false,
+    flex: 1,
+  },
+]
+
+const PokemonTable = ({ pokemons }: PokemonTableProps) => {
+  const navigate = useNavigate()
+
+  const rows = pokemons.map((pokemon) => {
+    return {
+      name: pokemon.name,
+      url: pokemon.url,
+    }
+  })
+
+  const handleRowClick: GridEventListener<'rowClick'> = (params) => {
+    navigate(`/pokemon/${params.row.url.slice(-2)}`)
+  }
+
+  return (
+    <Box sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={20}
+        rowsPerPageOptions={[20]}
+        getRowId={(row) => row.name}
+        disableSelectionOnClick
+        onRowClick={handleRowClick}
+      />
+    </Box>
+  )
+}
+
+export default PokemonTable
