c17077677b1f839afb037cc7953bffb6d509c50f
iortega-dev <iortegamancera@gmail.com>
2022-09-22 20:25:26 +0200

    feat: add useDebounce Hook

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+
+// taken from https://usehooks.com/useDebounce/
+export const useDebounce = (value: string, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [value, delay])
+  return debouncedValue
+}
