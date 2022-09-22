1bc766bce1ba4a808c2baa49d51f02b26747697c
iortega-dev <iortegamancera@gmail.com>
2022-09-22 23:58:47 +0200

    feat: add PokemonForms component

---
+++ src/screens/pokemon/components/PokemonForms/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Card, Typography } from '@mui/material'
+
+import { NamedAPIResource } from '../../../../types/Common'
+import usePokemonForms from '../../../../hooks/usePokemonForms'
+import { Status } from '../../../../api/pokeAPI'
+import PokemonFormTable from '../../../../components/PokemonFormTable'
+
+interface PokemonFormsProps {
+  forms: NamedAPIResource[]
+}
+
+const PokemonForms = ({ forms }: PokemonFormsProps) => {
+  const { pokemonForms, status } = usePokemonForms(forms)
+
+  return (
+    <Card sx={{ padding: 4 }} raised>
+      <Typography variant="h5" color="text.secondary" mb={2}>
+        Formas
+      </Typography>
+      {pokemonForms && !!pokemonForms.length && status === Status.SUCCESS && (
+        <PokemonFormTable forms={pokemonForms} />
+      )}
+    </Card>
+  )
+}
+
+export default PokemonForms
