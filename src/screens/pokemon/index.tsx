35ef9709aca31d27453abdb2af5afb38372d11cd
iortega-dev <iortegamancera@gmail.com>
2022-09-22 22:12:17 +0200

    feat: implements Pokemon load & display logic at <Pokemon />

--- src/screens/pokemon/index.tsx
+++ src/screens/pokemon/index.tsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+
+import { Grid } from '@mui/material'
+
+import DittoMessage from '../../components/DittoMessage'
+import { useSinglePokemon } from '../../hooks/useSinglePokemon'
+import PokemonCard from './components/PokemonCard'
+import PokemonMoves from './components/PokemonMoves'
 
 const Pokemon = () => {
-  return <div>Pokemon</div>
+  const { id } = useParams<{ id?: string }>()
+
+  if (!id) {
+    return <DittoMessage message="Es necesario un ID del pokemon" />
+  }
+
+  const { pokemonData } = useSinglePokemon(id)
+
+  if (!pokemonData) {
+    return (
+      <DittoMessage message="Lo sentimos, no existen datos para este pokemon" />
+    )
+  }
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={4}>
+        <PokemonCard data={pokemonData} />
+      </Grid>
+      <Grid item xs={8}>
+        <PokemonMoves moves={pokemonData.moves} />
+      </Grid>
+    </Grid>
+  )
 }
 
 export default Pokemon
